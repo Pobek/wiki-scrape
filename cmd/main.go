eb5ca21de1f8aceebcad0648166124afeab29b76
Evyatar Cohen <coevyatar@gmail.com>
2019-09-10 11:48:45 +0300

    Add verbose option

--- cmd/main.go
+++ cmd/main.go
@@ -12,13 +12,30 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "WikiScrape"
 	app.Usage = "Will scrape wikipedia for knowledge"
-	app.Action = func(c *cli.Context) error {
-		fmt.Println("WikiScrape test#1")
-		return nil
+
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name: "verbose",
+		},
 	}
+
+	app.Action = testOutput
 
 	err := app.Run(os.Args)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func testOutput(c *cli.Context) error {
+	name := "Marco"
+	if c.NArg() > 0 {
+		name = c.Args().Get(0)
+	}
+	if c.Bool("verbose") {
+		fmt.Println("Verbose enabled!")
+		fmt.Println("Will output hello <name>")
+	}
+	fmt.Printf("Hello %s", name)
+	return nil
 }
